4a83bb4666aff15149a9d95b8a2da5caf11d3ec5
Tim Spurway <tim@chango.com>
2012-04-23 15:43:21 -0400

    updated pid processing to use new datefile class

--- inferno/lib/pid.py
+++ inferno/lib/pid.py
@@ -31,28 +31,12 @@ class DaemonPid(object):
         return result
 
     def should_run(self, job):
-        last_run = Datefile(self._settings['pid_dir'], "%s.last_run" % job.rule_name)
+        last_run = Datefile(self._pid_dir, "%s.last_run" % job.rule_name)
         if last_run.is_older_than(job.rule.time_delta):
             return True
         else:
             log.debug('Skipping job: %s (last: %s)',
                 job.rule_name, last_run)
-
-
-#        path = self._last_run_path(job)
-#        if not os.path.exists(path):
-#            return True
-#        with open(path) as f:
-#            now = datetime.utcnow()
-#            delta = timedelta(**job.rule.time_delta)
-#            last_run = datetime.strptime(f.read().strip(), self._format)
-#            next_run = last_run + delta
-#            result = next_run < now
-#            if not result:
-#                log.debug('Skipping job: %s (last: %s, next: %s)',
-#                    job.rule_name, last_run.strftime(self._format),
-#                    next_run.strftime(self._format))
-#            return result
 
     def create_pid(self, job):
         path = self._get_pid_path(job)
@@ -62,21 +46,12 @@ class DaemonPid(object):
             f.write(str(os.getpid()))
         return True
 
-    def create_last_run(self, job):
-        path = self._last_run_path(job)
-        with open(path, 'w') as f:
-            now = datetime.utcnow()
-            f.write(now.strftime(self._format))
-
     def remove_pid(self, job):
         path = self._get_pid_path(job)
         os.unlink(path)
 
     def _get_pid_path(self, job):
         return os.path.join(self._pid_dir, '%s.pid' % job.rule_name)
-
-    def _last_run_path(self, job):
-        return os.path.join(self._pid_dir, '%s.last_run' % job.rule_name)
 
     @property
     def _pid_dir(self):
