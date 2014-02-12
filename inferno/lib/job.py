f55f64b46c34f9e0c2255f079a7f8062932be62b
Tait Clarridge <tait@chango.com>
2014-02-12 13:06:56 -0500

    Allowing result_tag_suffix to be date explicitly

--- inferno/lib/job.py
+++ inferno/lib/job.py
@@ -10,6 +10,7 @@ from inferno.lib.archiver import Archiver
 from inferno.lib.disco_ext import get_disco_handle
 from inferno.lib.job_options import JobOptions
 from inferno.lib.result import reduce_result
+from datetime import datetime
 
 log = logging.getLogger(__name__)
 
@@ -191,10 +192,13 @@ class InfernoJob(object):
             # sort the tags the job ran on, take the last part of the last tag
             # if that looks like a date, use it, otherwise use the job name
             if self.rule.result_tag_suffix:
-                tags = sorted(self.job_options.tags)
-                date = (tags[-1].split(':'))[-1]
-                if len(date) == 10 and '-' in date:
-                    suffix = date
+                if str(self.rule.result_tag_suffix).lower() == "date":
+                    suffix = str(datetime.now().date())
+                else:
+                    tags = sorted(self.job_options.tags)
+                    date = (tags[-1].split(':'))[-1]
+                    if len(date) == 10 and '-' in date:
+                        suffix = date
             tag_name = '%s:%s' % (self.job_options.result_tag, suffix)
             log.info('Tagging result: %s', tag_name)
             try:
