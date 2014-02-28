7fe88d28e2c86a884be81c7757cc8ffadff5aa71
Shayan Pooya <shayan@chango.com>
2014-02-28 15:17:08 -0500

    Remove the reference to README.

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ if sys.version_info[:2] < (2, 6):
     raise RuntimeError('Requires Python 2.6 or better')
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
 info = open(os.path.join(here, 'inferno', 'lib', '__init__.py')).read()
 VERSION = re.compile(r".*__version__ = '(.*?)'", re.S).match(info).group(1)
 
@@ -29,7 +28,6 @@ setup(
     name='inferno',
     version=VERSION,
     description=('Inferno: a python map/reduce platform powered by disco.'),
-    long_description=README,
     keywords='inferno discoproject',
     author='Chango Inc.',
     author_email='dev@chango.com',
